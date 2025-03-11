51cac17ff737b18f41dde659746f8d346816cd83
ian.burnside89@gmail.com <ian.burnside89@gmail.com>
2025-03-11 14:48:44 -0600

    setting up shadcd, inspiraui

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue';
 
 </script>
 
@@ -6,5 +7,6 @@
   <div class="flex justify-between">
     <h1 class="text-red-500"> Welcome to job goblin</h1>
     <h1>you filthy animal</h1>
+    <Button> Click Me </Button>
   </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { fileURLToPath, URL } from 'node:url';
-
+import path from 'node:path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
-
+import autoprefixer from 'autoprefixer';
+import tailwind from 'tailwindcss';
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
