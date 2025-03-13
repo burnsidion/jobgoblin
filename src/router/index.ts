3404a220af37203886bc9680c312fea91e973aa8
ian.burnside89@gmail.com <ian.burnside89@gmail.com>
2025-03-13 13:08:08 -0600

    setting up route guards

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import UserHomeView from '@/views/UserHomeView.vue';
+import { supabase } from '@/utilities/supabaseClient';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,8 +15,26 @@ const router = createRouter({
       path: '/home',
       name: 'homeView',
       component: UserHomeView,
+      meta: { requiresAuth: true },
     },
   ],
+});
+
+router.beforeEach(async (to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+
+    if (!session?.user) {
+      console.warn('🚨 Unauthorized access attempt! Redirecting to home.');
+      next('/');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
